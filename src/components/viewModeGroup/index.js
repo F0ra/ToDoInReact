c3424940e65c82575c9963537a0a996a20c4408c
F0ra <forexample.fora@gmail.com>
2018-05-16 01:09:44 +0300

    code eleven

--- src/components/viewModeGroup/index.js
+++ src/components/viewModeGroup/index.js
@@ -5,12 +5,16 @@ import "./viewModeGroup.css"
 export const ViewModeGroup = (props)=>{
     return(
         <div className="dropdown view-mode-group">
-            <button className="btn btn btn-light dropdown-toggle dropdown-menu-button-small" type="button"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                
-            </button>
+            
             <button className="btn btn btn-light dropdown-toggle dropdown-menu-button-defolt" type="button"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Show...
             </button>
+
+            <button className="btn btn btn-light dropdown-toggle dropdown-menu-button-small" type="button"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                
+            </button>
+
+            
             <div className ="dropdown-menu" aria-labelledby="dropdownMenuButton">
                 <a  className={"dropdown-item view view-mode-options" + (props.viewMode==="view All" ? " active" : " ")}  onClick={props.actions.viewAll} >show All</a>
                 <a  className={"dropdown-item view view-mode-options" + (props.viewMode==="view Done" ? " active" : " ")}  onClick={props.actions.viewDone}>show Done</a>
