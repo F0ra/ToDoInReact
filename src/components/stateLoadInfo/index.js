4bb11e4e96aa2391af881a0db56c2a359115f4fe
F0ra <forexample.fora@gmail.com>
2018-05-16 03:40:10 +0300

    code 1413

--- src/components/stateLoadInfo/index.js
+++ src/components/stateLoadInfo/index.js
@@ -4,9 +4,14 @@ import './stateLoadInfo.css';
 
 export const StateLoadInfo = (props)=>{
     return(
+        <React.Fragment>
+        <div className="state-load-info-background">
+        
+        </div>
         <div className="alert alert-success state-load-info" role="alert">
             <h4 className="alert-heading">Good news!</h4>
             <p>Todo`s state has been loaded from localStorage</p>
         </div>
+        </React.Fragment>
     );
 };
